4388014b95e7e2996a3549ec0cc65bb5fa6ce300
manu911 <elmanu911@gmail.com>
2024-01-28 10:18:41 +0100

    select-none

--- trilo/src/app/newTick.tsx
+++ trilo/src/app/newTick.tsx
@@ -15,9 +15,9 @@ export default function NewTick(props: { checked?, addTick }) {
     }
     return (
         <div className="flex flex-nowrap">
-            <span className="text-nowrap font-bold text-lg text-red-700 mt-0.5">{checked ? "X " : "O "}</span><span className="mt-0.5">&nbsp;|</span>
+            <span className="text-nowrap font-bold text-lg text-red-700 mt-0.5 select-none">{checked ? "X " : "O "}</span><span className="mt-0.5 select-none">&nbsp;|</span>
             <input type="text" placeholder="Add a new Tick" onKeyDown={keydownHandler} className="ml-2 px-2 rounded-md placeholder-slate-500 w-full "></input>
-            <button onClick={click} className="ml-3 rounded-full p-1 text-sm border-white border-2 border-solid ">Add</button>
+            <button onClick={click} className="ml-3 rounded-full p-1 text-sm border-white border-2 border-solid select-none">Add</button>
         </div>
     )
 }
